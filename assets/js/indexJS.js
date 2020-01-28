07c4359c467a037e6b9fde8d4f691a40c27d5a42
edvinas <edvinas000@hotmail.com>
2020-01-28 17:35:49 +0200

    frontend checkboxes added

--- assets/js/indexJS.js
+++ assets/js/indexJS.js
@@ -5,14 +5,25 @@ form.addEventListener('submit',(event)=>{
     event.preventDefault();
     const raktinisZodis = form.raktinisZodis.value;
     const miestas = form.miestas.value;
-
-    $.ajax({
-        method:'POST',
-        url: '/paieska',
-        data: {raktinis: raktinisZodis,
-        miestas: miestas},
-        success: function(data){
-            console.log(data);
-        }
-    })
+    const cvBCheck = form.cvBankas;
+    const cvMCheck = form.cvMarket;
+    const cvCheck = form.cv;
+    const cvKCheck = form.cvKodas;
+    const checkboxesArr = [cvBCheck,cvMCheck,cvCheck,cvKCheck]
+     let checkedArr =[];
+     checkboxesArr.forEach(checkbox => {
+         if(checkbox.checked){
+             checkedArr.push(checkbox.name)
+         }
+     });
+     console.log(checkedArr);
+    // $.ajax({
+    //     method:'POST',
+    //     url: '/paieska',
+    //     data: {raktinis: raktinisZodis,
+    //     miestas: miestas},
+    //     success: function(data){
+    //         console.log(data);
+    //     }
+    // })
 });
