7dcdfe38ef6711fdd2375ce0ed549652509e762c
Edvinas <edvinas2121@hotmail.com>
2020-04-26 16:38:00 +0300

    added basic responsive design

--- mongoDB/connection.js
+++ mongoDB/connection.js
@@ -1,16 +1,13 @@
-const AdListModel = require('./schema');
 const connectMongoDB = (mongoose)=>{
     if (process.env.NODE_ENV !== 'prod') {
         const dotenv = require('dotenv');
-        dotenv.config({
-            path: './.env'
-        });
+        dotenv.config();
     };
     if(process.env.NODE_ENV === 'dev'){
         console.log('mongo connection about to start');
     mongoose.connect('mongodb://localhost/users', { useNewUrlParser: true, useUnifiedTopology: true });
     }else if (process.env.NODE_ENV === 'prod'){
-        // connect to online db
+        mongoose.connect(process.env.MONGO_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true });
     }
     mongoose.connection.once('open',()=>{
         console.log('mongo connection has been made');
